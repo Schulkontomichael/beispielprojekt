a2eb230111e531a594cd7d8615d4970a757edc7d
Schulkontomichael <152477766+Schulkontomichael@users.noreply.github.com>
2023-11-30 09:12:54 +0000

    first commit

---
+++ tsdev/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+class Main {
+    static sayHello(n) {
+        for (let i = 0; i < n; i++) {
+            console.log("Hello World");
+        }
+    }
+}
+Main.sayHello(10);
+//# sourceMappingURL=index.js.map
---
+++ tsdev/src/index.ts
@@ -0,0 +1,9 @@
+class Main{
+    static sayHello(n:number){
+        for (let i=0; i<n; i++){
+        console.log("Hello World");
+        }
+    }
+}
+
+Main.sayHello(10);
